700a977b9f9edc97e01e1e7661b17881c882251c
leixiaoda <leixiaoda@gmail.com>
2018-07-03 15:00:19 +0800

    fix: change ways to encoding

--- SwiftPlusCPP/ViewController.swift
+++ SwiftPlusCPP/ViewController.swift
@@ -14,10 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
-        let json = ConverterWrapper.markdownToJson("# title")
+        let json = ConverterWrapper.markdownToJson("来")
         print("json:")
         print(json)
-        let markdown = ConverterWrapper.jsonToMarkdown("{\n    \"author\": \"\",\n    \"blocks\": [\n        {\n            \"id\": \"TextBlock_vp52m8oqz10ho\",\n            \"index\": 0,\n            \"text\": \"title_h1\",\n            \"type\": \"h1\"\n        },\n    {\n            \"id\": \"TextBlock_vp52m8oqz10ho\",\n            \"index\": 0,\n            \"text\": \"title_h3\",\n            \"type\": \"h3\"\n        }\n    ],\n    \"createTime\": 0,\n    \"folderId\": 0,\n    \"id\": \"\",\n    \"lastModify\": 0,\n    \"version\": \"\"\n}")
+        let markdown = ConverterWrapper.jsonToMarkdown("{\n    \"author\": \"\",\n    \"blocks\": [\n        {\n            \"id\": \"TextBlock_vp52m8oqz10ho\",\n            \"index\": 0,\n            \"text\": \"来\",\n            \"type\": \"h1\"\n        },\n    {\n            \"id\": \"TextBlock_vp52m8oqz10ho\",\n            \"index\": 0,\n            \"text\": \"title_h3\",\n            \"type\": \"h3\"\n        }\n    ],\n    \"createTime\": 0,\n    \"folderId\": 0,\n    \"id\": \"\",\n    \"lastModify\": 0,\n    \"version\": \"\"\n}")
         print("markdown:")
         print(markdown)
     }
